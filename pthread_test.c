ca74b7d65e423f6fde1e676b2ff01d023192a512
Antti Kantee <pooka@iki.fi>
2014-06-26 00:24:15 +0000

    test timedwait

--- pthread_test.c
+++ pthread_test.c
@@ -89,28 +89,42 @@ wakeupthread(void *arg)
 int
 main(int argc, char **argv)
 {
+	struct timespec ts;
 	pthread_t pt;
-	int rounds = 0;
+
+	pthread_mutex_init(&mtx, NULL);
+	pthread_cond_init(&cv, NULL);
+	pthread_cond_init(&cv2, NULL);
 
 	if (pthread_create(&pt, NULL, mythread, (void *)0x01) != 0)
 		errx(1, "pthread_create()");
+
+	clock_gettime(CLOCK_REALTIME, &ts);
+	ts.tv_nsec += 1000*1000;
+	pthread_mutex_lock(&mtx);
+	if (pthread_cond_timedwait(&cv2, &mtx, &ts) != ETIMEDOUT) {
+		printf("cond_timedwait fail\n");
+		abort();
+	}
+	pthread_mutex_unlock(&mtx);
+
 	if (pthread_create(&pt, NULL, mythread, (void *)0x02) != 0)
 		errx(1, "pthread_create()");
 	if (pthread_create(&pt, NULL, waitthread, (void *)0x03) != 0)
 		errx(1, "pthread_create()");
 	if (pthread_create(&pt, NULL, wakeupthread, (void *)0x04) != 0)
 		errx(1, "pthread_create()");
-	pthread_mutex_init(&mtx, NULL);
-	pthread_cond_init(&cv, NULL);
-	pthread_cond_init(&cv2, NULL);
 
 	pthread_mutex_lock(&mtx);
+	/* get time after locking => ensure loop runs before threads finish */
+	clock_gettime(CLOCK_REALTIME, &ts);
+	ts.tv_sec++;
 	while (nthreads) {
+		int rv;
+
 		printf("mainthread condwaiting\n");
-		pthread_cond_wait(&cv2, &mtx);
-		if (rounds++ > 100) {
-			printf("condwait broken?\n");
-			exit(1);
+		if ((rv = pthread_cond_timedwait(&cv2, &mtx, &ts)) != 0) {
+			printf("drain condwait fail %d %d\n", rv, nthreads);
 		}
 	}
 	pthread_mutex_unlock(&mtx);
